94bca3d189a8c8798fc015367ed2e8b10e3787f5
Melanie <melanie@Stacys-MBP.localdomain>
2021-03-19 13:21:17 -0700

    update readme, add comments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import formData from './exampleData';
 const { fields: formFields } = formData;
 
 class Form extends React.Component {
+  // Initializes the state object to contain an empty string for
+  // each field in the form, and binds all internal functions.
+  // State object example:
+  // {
+  //   "first_name": "",
+  //   "last_name": "",
+  //   "email": "",
+  //   "phone_number": "",
+  //   "job_title": "",
+  //   "date_of_birth": "",
+  //   "parental_consent": ""
+  // }
   constructor(props) {
     super(props);
     this.state = {};
@@ -16,6 +28,11 @@ class Form extends React.Component {
     this.generateInputElement = this.generateInputElement.bind(this);
   }
 
+  // Handles all form changes by setting the state of a given
+  // form field to the value entered by the user.
+  // If the date field is changed, calls the handleParentalConsent
+  // function to choose whether to display the parental consent
+  // checkbox.
   handleChange(event) {
     const stateObj = {};
     stateObj[event.target.id] = event.target.value;
@@ -25,12 +42,19 @@ class Form extends React.Component {
     }
   }
 
+  // When the user submits the form, logs all form items and values,
+  // prevents the form from clearing, and returns the state object.
   handleSubmit(event) {
     console.log('Form was submitted: ', this.state);
     event.preventDefault();
     return this.state;
   }
 
+  // When the Date of Birth field is updated, checks if the date entered
+  // is less than 13 years prior to today's date. If it is, removes the
+  // 'd-none' class, telling bootstrap to display the div containing the
+  // parental consent checkbox. If the date is subsequently changed to be
+  // greater than 13 years ago, re-hides the checkbox.
   handleParentalConsent(event) {
     const date = new Date(event.target.value);
     const now = new Date();
@@ -45,12 +69,18 @@ class Form extends React.Component {
     }
   }
 
+  // Generatess an input element given a field.
   generateInputElement(field, i) {
+    // Checks if the field has a conditional element (indicating that
+    // it shouldn't be displayed immediately). If so, sets a variable
+    // to 'd-none' to be added as a class later.
     const isConditional = field.hasOwnProperty('conditional');
     let hidden = '';
     if (isConditional) {
       hidden = 'd-none';
     }
+    // Sets the class for displaying inputs. For a checkbox field,
+    // sets a different class than all other fields.
     let inputClass = 'form-control';
     if (field.type === 'checkbox') {
       inputClass = 'form-check-input';
@@ -65,6 +95,8 @@ class Form extends React.Component {
     );
   }
 
+  // Creates the form and submit button; generates one form input
+  // element per field provided in exampleData.json.
   render() {
     return (
       <div className="container">
